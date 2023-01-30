48b0d385d7815966727e2263a3072b6cc14ad4db
harshit Mishra <harshitmishra1255@gmail.com>
2023-01-30 22:20:48 +0530

    theme changed and code cleaned

--- flasher/lib/main.dart
+++ flasher/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:record/record.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:torch_light/torch_light.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
@@ -30,48 +28,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _counter = 0;
   final _audioRecorder = Record();
   final recorder = FlutterSoundRecorder();
-  bool permission = false;
-  void _incrementCounter(value) {
-    setState(() {
-      _counter = value;
-    });
-  }
+  bool button_pressed = false;
+  static const onpressed_button_color = Colors.red;
+  static const normal_button_color = Colors.white;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            TextButton(
-                onPressed: () async {
-                  if (!permission) {
-                    await _audioRecorder.start();
-                    setState(() {
-                      permission = true;
-                    });
-                    await _audioRecorder
-                        .onAmplitudeChanged(Duration(milliseconds: 50))
-                        .listen((event) {
-                      var a = event;
-                      if (a.current >= -20) {
-                        TorchLight.enableTorch();
-                      } else {
-                        TorchLight.disableTorch();
-                      }
-                      _incrementCounter(a.current);
-                    });
-                  } else {
-                    permission = false;
-                    _audioRecorder.stop();
-                  }
-                },
-                child: const Icon(Icons.start)),
-            Text('$_counter')
+            Icon(
+              Icons.flash_on,
+              color: button_pressed ? Colors.white : Colors.black,
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    button_pressed ? normal_button_color : onpressed_button_color),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                    )),
+              ),
+              onPressed: () async {
+                if (!button_pressed) {
+                  await _audioRecorder.start();
+                  setState(() {
+                    button_pressed = true;
+                  });
+                  _audioRecorder
+                      .onAmplitudeChanged(const Duration(milliseconds: 50))
+                      .listen((event) {
+                    var amplitude = event;
+                    if (amplitude.current >= -20) {
+                      TorchLight.enableTorch();
+                    } else {
+                      TorchLight.disableTorch();
+                    }
+                  });
+                } else {
+                  setState((){
+                    button_pressed = false;
+                  });
+                  _audioRecorder.stop();
+                }
+              },
+              child: Text(button_pressed ? 'OFF' : 'ON',
+                  style: const TextStyle(color: Colors.black,fontWeight: FontWeight.w600)),
+            ),
           ],
         ),
       ),
